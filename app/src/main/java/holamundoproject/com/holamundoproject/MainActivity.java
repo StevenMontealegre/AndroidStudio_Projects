7f87ba1dce4fec447f8266a4978fc12ee922c1dd
StevenMontealegre <jamessteven8956>
2019-01-31 20:59:44 -0500

    Finally Changes v2

--- app/src/main/java/holamundoproject/com/holamundoproject/MainActivity.java
+++ app/src/main/java/holamundoproject/com/holamundoproject/MainActivity.java
@@ -7,16 +7,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_reg2;
+    private Button btn_login;
+    private EditText et_user, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_user = findViewById(R.id.et_user);
+        et_password = findViewById(R.id.et_password);
+
+
         tv_reg2 = (TextView) findViewById(R.id.tv_reg2);
         tv_reg2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Profile.class);
+                i.putExtra("nombre", et_user.getText().toString());
+                i.putExtra("contra", et_password.getText().toString());
+                startActivity(i);
+            }
+        });
 
         builder.show();
     }
